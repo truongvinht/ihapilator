873f8131e78aaab231f052e52b2ed97d874a85d8
Vinh Tran <truongvinht@gmail.com>
2016-07-02 14:32:27 +0200

    Initial Commit

---
+++ iHapilator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iHapilator
+//
+//  Created by Vinh Tran on 02/07/16.
+//  Copyright © 2016 Truong Vinh Tran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
